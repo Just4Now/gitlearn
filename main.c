67e1078b75af2d115ee4ea313650ce91f1059fcf
young713 <young713@126.com>
2019-03-19 11:45:44 +0800

    rm python

--- main.c
+++ main.c
@@ -1,8 +1,44 @@
-#include<stdio.h>
-#include<arpa/inet.h>
-
+#include <stdio.h>
+#include <termios.h>
+#include <string.h>
 int main()
 {
-    printf("hello world");
-    return 0;
+int c;
+int w,h;
+struct termios new_setting,init_setting;
+   
+    if (tcgetattr(0, &init_setting) != 0)
+    {
+        printf("Cannot get the attribution of the terminal.");
+        goto quit;
+    }
+memcpy(&new_setting, &init_setting, sizeof(struct termios));
+
+    new_setting.c_lflag &= ~(ICANON | ECHO);;
+    if (tcsetattr(0, TCSANOW, &new_setting) != 0)
+    {
+        printf("Cannot set the attribution of the terminal.");
+        goto quit;
+    }
+                do
+                {
+                    printf("---- Enter space to display more records or 'q' to quit. ----\n");
+                    c = getchar();
+if (c == 'q') {
+                        goto quit;
+                    }else if (c == ' ') {
+                        break;
+                    }else
+                    {
+                        /* do nothing */
+                    }                    
+
+
+                } while (c != EOF);
+quit:
+if (tcsetattr(0, TCSANOW, &init_setting) != 0)
+    {
+        printf("Cannot set the attribution of the terminal.");
+    }
+return 0;
 }
