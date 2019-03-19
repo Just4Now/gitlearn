d3607c089f2523eefbd9e8312bc93c76eb36e64f
young713 <young713@126.com>
2019-03-19 11:49:28 +0800

    rm python

--- python/Readme.txt
+++
@@ -1,4 +0,0 @@
-learn python3
-learn git branch
-learn bug branch
-learn github operation
--- python/test.py
+++
@@ -1,7 +0,0 @@
-from function import to_timestamp
-from function import safe_base64_decode
-
-# 测试:
-assert b'abcd' == safe_base64_decode(b'YWJjZA=='), safe_base64_decode('YWJjZA==')
-assert b'abcd' == safe_base64_decode(b'YWJjZA'), safe_base64_decode('YWJjZA')
-print('Pass')
